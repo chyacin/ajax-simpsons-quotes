12fd44213fd5370c60c5d450bbe52e32c43a666c
Calvin Hyacinth <46787710+chyacin@users.noreply.github.com>
2021-04-12 16:39:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function fetchSimpsonJSON() {
 
 
-    const simpsons =[
+    const simpsons = [
         {
-         "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
-         "character": "Nelson Muntz",
-         "image" : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
-         "characterDirection" : "Left"
+            quote: "By chilling my loins I increase the chances of impregnating my wife.",
+            character: "Apu Nahasapeemapetilon",
+            image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+            characterDirection: "Left"
         }
-      ]
+    ]
         ;
 
     const url = 'https://simpsons-quotes-api.herokuapp.com/quotes';
